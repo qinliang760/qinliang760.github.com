77047073296d2a28ff84db36c73461fe47134793
qinliang <qinliang760@163.com>
2014-02-27 18:47:43 +0800

    modify to jquery

--- NESH/jquery.bracket.js/js/jquery.bracket.js
+++ NESH/jquery.bracket.js/js/jquery.bracket.js
@@ -22,30 +22,66 @@
             results=defaults.data.results;
 
         $.extend(this, {
-            setTeams: function() {
-                var resultsL=results.length;
-                for(var i=0,l=resultsL;i<l;i++){
-                    
-                }
+            setTeams: function(num) {
+
+                var wrap=$('<div class="jQBracket"></div>').appendTo($obj);
+
+                var height = teams.length * 64;
+                var wEl=$('<div class="bracket"></div>').appendTo(wrap);
+                wEl.css("height",height);
+/*                var rounds = Math.log(teams.length * 2) / Math.log(2);
+                var matches = teams.length;*/
+
+
+                var results_point=results[0];
+                var teamsArr=[];
                 for(var i=0,l=teams.length;i<l;i++){
-                    <div class="match">
-                        <div class="teamContainer">
-                            <div class="team">teamwrap</div>
-                                <div class="label">teams[i][0]</div>
-                                <div class="score">results[i][0]</div>
-                            </div>
-                            <div class="team">teamwrap</div>
-                                <div class="label">teams[i][1]</div>
-                                <div class="score">results[i][1]</div>
-                            </div>
-                            <div class="connector"></div>
-                        </div>
-                    </div>                    
+                    teamsArr.push('<div class="match">'+
+                        '<div class="teamContainer">'+
+                            '<div class="team">teamwrap</div>'+
+                                '<div class="label">'+teams[i][0]+'</div>'+
+                                '<div class="score">'+results_point[i][0]+'</div>'+
+                            '</div>'+
+                            '<div class="team">teamwrap</div>'+
+                                '<div class="label">'+teams[i][1]+'</div>'+
+                                '<div class="score">'+results_point[i][1]+'</div>'+
+                            '</div>'+
+                            '<div class="connector"></div>'+
+                        '</div>'+
+                    '</div>');                    
 
 
-                }      
+                }
+                return '<div class="round">'+
+                //this.getWinnerTeams();
             },
-            getTeams:function(){
+            getWinnerTeams:function(num){
+                var results_point=results[0];   //第二轮比赛结果
+                var teams2=[];
+                for(var i=0,l=results_point.length;i<l;i++){
+                    if(results_point[i][0]>results_point[i][1]){
+                        teams2.push(0);
+                    }else{
+                       teams2.push(1); 
+                    }
+                }
+
+
+                var teams2_2_j=[],teams2_2_o=[];
+                for (var j=0,jl=teams2.length;j<jl;j++){
+                    if(j%2==0){
+                        teams2_2_o.push(teams[j][teams2[j]]);
+                    }else{
+                        teams2_2_j.push(teams[j][teams2[j]]);
+                    }
+                }
+
+                var teams2_2=[];
+                for (var m=0,ml=teams2_2_j.length;m<ml;m++){
+                    teams2_2[m]=[teams2_2_j[m],teams2_2_o[m]];
+                }
+
+
 
             },
             setPosition: function() {
@@ -54,7 +90,11 @@
             },
             
             init: function() {
-
+                //this.setTeams();
+                this.setTeams(1);
+                this.setTeams(2);
+                this.setTeams(3);
+                this.setTeams(4);
             }
         })
         this.init();
