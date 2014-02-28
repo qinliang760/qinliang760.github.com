3f0311a82487c3d27c2564f74a7a17b79bcb3a41
qinliang <qinliang760@163.com>
2014-02-28 19:10:06 +0800

    modify bracket

--- NESH/jquery.bracket.js/js/jquery.bracket.js
+++ NESH/jquery.bracket.js/js/jquery.bracket.js
@@ -34,17 +34,38 @@
                 //var matches = teams.length;
 
 
-                var results_point=results[num];
-                var team=this.getTeams(num);debugger;
+                
+                if(num==null){
+                    var team=teams;
+                    var results_point=results[0];
+                }else{
+                    var team=this.getTeams(num);
+                    var results_point=results[num];  
+                }//debugger;
+
+                
                 var teamsArr=[];
+                if(typeof team=="string"){
+                    teamsArr.push('<div class="match">'+
+                        '<div class="teamContainer">'+
+                            '<div class="team">'+
+                                '<div class="label">'+team+'</div>'+
+                            '</div>'+
+                            '<div class="connector"></div>'+
+                        '</div>'+
+                    '</div>');
+                    return teamsArr.join(""); 
+                }
+
+                
                 for(var i=0,l=team.length;i<l;i++){
                     teamsArr.push('<div class="match">'+
                         '<div class="teamContainer">'+
-                            '<div class="team">teamwrap</div>'+
+                            '<div class="team">'+
                                 '<div class="label">'+team[i][0]+'</div>'+
                                 '<div class="score">'+results_point[i][0]+'</div>'+
                             '</div>'+
-                            '<div class="team">teamwrap</div>'+
+                            '<div class="team">'+
                                 '<div class="label">'+team[i][1]+'</div>'+
                                 '<div class="score">'+results_point[i][1]+'</div>'+
                             '</div>'+
@@ -60,19 +81,26 @@
             getTeams:function(num){
                 var results_point=results[num];   //第二轮比赛结果
                 var teams2=[];
+                var teams2_2=[];
                 for(var i=0,l=results_point.length;i<l;i++){
                     if(results_point[i][0]>results_point[i][1]){
                         teams2.push(0);
                     }else{
                        teams2.push(1); 
                     }
-                }
+                }debugger;
 
                 if(num==0){
                     var teamG=teams;
                 }else{
                     var teamG=teamsGroup[num-1];
                 }
+
+                if(teams2.length==1){
+                    //teams2_2[0]=teamG[0][teams2[0]];
+                    return teamG[0][teams2[0]];
+                }
+
                 var teams2_2_j=[],teams2_2_o=[];
                 for (var j=0,jl=teams2.length;j<jl;j++){
                     if(j%2==0){
@@ -81,38 +109,37 @@
                         teams2_2_j.push(teamG[j][teams2[j]]);
                     }
                 }
-//debugger;
+
                 if(!teams2_2_j.length){
                     teamsGroup[num]=teams2_2_o;
                     return teams2_2;
                 }
-                var teams2_2=[];
+                //var teams2_2=[];
                 for (var m=0,ml=teams2_2_o.length;m<ml;m++){
                     teams2_2[m]=[teams2_2_o[m],teams2_2_j[m]];
                 }
 
-                teamsGroup[num]=teams2_2;debugger;
+                teamsGroup[num]=teams2_2;
                 return teams2_2;
 
 
             },
             setRounds: function() {
                 var rounds = Math.log(teams.length * 2) / Math.log(2);
+                var teamInit=self.setTeams(null);
                 var roundsArr=[];
-                for(var i=0,l=rounds;i<l-1;i++){
+                roundsArr.push(teamInit);
+                for(var i=0,l=rounds;i<l;i++){
                     var team=self.setTeams(i);
                     roundsArr.push(team);
                 }debugger;
+                
 
             },
             
             init: function() {
                 this.setRounds();
-                //this.setTeams();
-                //this.setTeams(1);
-                //this.setTeams(2);
-                //this.setTeams(3);
-                //this.setTeams(4);
+
             }
         })
         this.init();
