bc85eb17869142a64d629263aa1076dbf80f0840
qinliang <qinliang760@163.com>
2014-03-03 16:35:00 +0800

    add grunt method

---
+++ grunt_main/Gruntfile.js
@@ -0,0 +1,54 @@
+/*
+ * grunt
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * https://github.com/gruntjs/grunt/blob/master/LICENSE-MIT
+ */
+
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+
+		useminPrepare: {//all html
+			html: 'E:/myfile/git_new/qinliang760.github.com/grunt/project/html/js.tpl',
+			options: {
+			      root: 'E:/myfile/git_new/qinliang760.github.com/grunt/project/',
+			      dest:"E:/myfile/git_new/qinliang760.github.com/grunt/project/"
+			}			
+		},
+		usemin: {
+			html: ['html/js.tpl']
+		},
+		cssmin: {//all css dir and sub dir
+			minify: {
+		        expand: true,
+		        cwd   : 'project/css/',
+		        src   : ['**/*.css', '!*.min.css'],
+		        dest  : 'project/css/',
+		        ext   : '.min.css'				
+			}
+		},
+		uglify: {//all css dir and sub dir
+	      production : {
+	        src     : ['**/*.js'],
+	        cwd     : 'project/js/',
+	        dest    : 'project/js/',
+	        expand  : true,
+	        ext     : '.min.js',
+	      }
+		}				
+
+	})
+
+ 	grunt.loadNpmTasks('grunt-usemin');
+ 	grunt.loadNpmTasks('grunt-contrib-concat');
+ 	grunt.loadNpmTasks('grunt-contrib-uglify');
+ 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  //grunt.registerTask('default', ['useminPrepare','concat', 'uglify', 'cssmin','usemin']);
+  grunt.registerTask('default', ['cssmin','uglify']);
+}
+
