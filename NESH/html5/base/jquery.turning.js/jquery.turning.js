0475794262fd61c359d72c01e9eaa7d6f782f387
qinliang <qinliang760@163.com>
2014-11-13 11:31:54 +0800

    the great vision for jquery.turning.js

--- NESH/html5/base/jquery.turning.js/jquery.turning.js
+++ NESH/html5/base/jquery.turning.js/jquery.turning.js
@@ -14,10 +14,14 @@ $.ns("JV");
      */
     JV.turning = function(Jnode, cfg) {
         var defaults = {
+            effect:"scaleAndTranslate",//scaleAndTranslate,translate
+            callback:function(index){
 
+            }
         };
         this.items = $(Jnode);//html or $
         this.cfg = $.extend({}, defaults, cfg); //合并defaults和cfg，不修改defaults
+        this.callback=this.cfg.callback;
         this.init();
 
     };
@@ -58,6 +62,7 @@ $.ns("JV");
 
             self.items.bind("touchmove",function(e){
                 e.preventDefault();
+
                 var target=e.targetTouches[0],
                     touch_y=target.pageY,
                     run_y=Math.abs(touch_y-self.startY),
@@ -66,120 +71,144 @@ $.ns("JV");
                     h=1-run_y/self.wH,//剩余滑动的百分比
                     i = self.wH * h;//剩余滑动的高度值
 
+ 
                 if (touch_y - self.startY <= 0) {
                     if (c_index >= sectoinL - 1) {
                         return;
                     }
-                    $(".m-section" + (c_index+1)).css({
+                    $(".m-section" + (c_index + 1)).css({
                         "-webkit-transform-origin": "50% 0px",
                         "-webkit-transform": "scale(" + h + ")"
-                    }), $(".m-section" + (c_index+2)).css({
+                    }), $(".m-section" + (c_index + 2)).css({
                         "-webkit-transform": "translateY(" + i + "px)"
                     })
-                }else{
-                    if(c_index!=0){
-                        $(".m-section" + (c_index+1)).css({
+                } else {
+                    if (c_index != 0) {
+                        $(".m-section" + (c_index + 1)).css({
                             "-webkit-transform-origin": "50% 100%",
                             "-webkit-transform": "scale(" + h + ")"
                         }), $(".m-section" + c_index).css({
                             "-webkit-transform": "translateY(-" + i + "px)"
-                        })                        
+                        })
                     }
 
                 }
+
             })
             
             self.items.bind("touchend",function(e){
                 e.preventDefault();
+                if(self.animated){
+                    return;
+                }
+                self.animated=true;//松开手指滑动开始动画
                 var target=e.changedTouches[0],
                     touch_y=target.pageY,
                     run_y=Math.abs(touch_y-self.startY),
                     c_index=self.index,//当前的索引
                     sectoinL=self.items.length;
-
+                    
                 //sectoinL/4 屏幕的四分之一    
                 if (touch_y - self.startY <= 0) {
 
                     if(c_index ==sectoinL - 1 ){//最后一张
+                        self.animated=false;
                         return;
                     } 
 
                     if(run_y>=self.wH/4){
-                        $(".m-section" + (c_index + 1)).animate({
-                            "-webkit-transform": "scale(0)"
-                        }, self.speed, function() {
-                            $(this).css({
-                                "-webkit-transform": "scale(1) translateY(-" + self.wH + "px)"
-                            });
-                            self.index++;
 
-                        });
-                        $(".m-section" + (c_index + 2)).animate({
-                            "-webkit-transform": "translateY(0px)"
-                        }, self.speed,function(){
+                        self.setScaleSmall(c_index + 1,"up");
+                        self.setTranslateSmall(c_index + 2,"up")
 
-                        });
                     }else{
-                        $(".m-section" + (c_index + 1)).animate({
-                            "-webkit-transform": "scale(1)"
-                        }, self.speed, function() {
-                            $(this).css({
-                                "-webkit-transform": "scale(1) translateY(0px)"
-                            });
 
-                        });
-                        $(".m-section" + (c_index + 2)).animate({
-                            "-webkit-transform": "translateY(" + self.wH + "px)"
-                        }, self.speed, function() {
+                        self.setScaleLarge(c_index + 1,"up");
+                        self.setTranslateLarge(c_index+2,"up");
 
-                        });
                     }
  
                 }else{
                     if(c_index ==0 ){//第一张
+                        self.animated=false;
                         return;
                     }   
 
                     if(run_y>=self.wH/4){
-                        $(".m-section" + c_index).animate({
-                            "-webkit-transform": "translateY(0px)"
-                        }, self.speed, function() {
-                            $(this).css({
-                                "-webkit-transform": "scale(1) translateY(0px)"
-                            });
-                            self.index--;
 
-                        });
-                        $(".m-section" + (c_index + 1)).animate({
-                            "-webkit-transform": "scale(0)"
-                        }, self.speed, function() {
-                            $(this).css({
-                                "-webkit-transform": "scale(1) translateY(" + self.wH + "px)"
-                            });
-                        });
+                        self.setScaleSmall(c_index + 1,"down");
+                        self.setTranslateSmall(c_index,"down");
+
                     }else{
-                        $(".m-section" + c_index).animate({
-                            "-webkit-transform": "scale(0px)"
-                        }, self.speed, function() {
-                            $(this).css({
-                                "-webkit-transform": "scale(1) translateY(" + self.wH + "px)"
-                            });
 
-                        });
-                        $(".m-section" + (c_index + 1)).animate({
-                            "-webkit-transform": "scale(1)"
-                        }, self.speed, function() {
-                            $(this).css({
-                                "-webkit-transform": "scale(1) translateY(0px)"
-                            });
-                        });
+                        self.setScaleLarge(c_index + 1,"down");
+                        self.setTranslateLarge(c_index,"down");
 
                     }
 
                 }                    
-            })
+            })//touchend
 
 
+        },
+        setScaleSmall:function(index,dir){//索引，方向(up or down)
+            var self=this;         
+            $(".m-section" + index).animate({
+                "-webkit-transform": "scale(0)"
+            }, self.speed, function() {
+                $(this).css({
+                    "-webkit-transform": "scale(1) translateY("+ self.wH + "px)"
+                });
+                if(dir=="up"){
+                    self.index++;
+                }                
+                
+                self.animated=false;//动画结束
+                self.callback(index);//setion动画结束后，内部元素需执行的动画
+            });
+        },
+        setScaleLarge:function(index,dir){
+            var self=this;
+            $(".m-section" + index).animate({
+                "-webkit-transform": "scale(1)"
+            }, self.speed, function() {
+                $(this).css({
+                    "-webkit-transform": "scale(1) translateY(0px)"
+                });
+                self.animated=false;
+
+            });
+
+        },
+        setTranslateSmall:function(index,dir){
+            var self=this;
+            $(".m-section" + index).animate({
+                "-webkit-transform": "translateY(0px)"
+            }, self.speed, function() {
+                $(this).css({
+                    "-webkit-transform": "scale(1) translateY(0px)"
+                });
+                if(dir=="down"){
+                    self.index--;
+                } 
+                self.animated=false;               
+
+            });
+        },
+        setTranslateLarge:function(index,dir){
+            var self=this;
+            var dir_v="";
+            if(dir=="down"){
+                dir_v="-";
+            }            
+            $(".m-section" + index).animate({
+                "-webkit-transform": "translateY("+dir_v + self.wH + "px)"
+            }, self.speed, function() {
+                $(this).css({
+                    "-webkit-transform": "scale(1) translateY(" + self.wH + "px)"
+                });
+                self.animated=false;
+            });
         }
 
   };
